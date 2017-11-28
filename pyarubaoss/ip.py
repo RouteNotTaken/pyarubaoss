14f8e132f3ab17d109bf724568c2b498e8d35cc9
Nehemiah Hernandez <nrhernandez@scoe.net>
2017-11-28 10:25:31 -0800

    Added set and del functions to ip.py.

--- pyarubaoss/ip.py
+++ pyarubaoss/ip.py
@@ -33,3 +33,34 @@ def get_iproutes(auth):
         return iproutes
     except requests.exceptions.RequestException as error:
         return "Error:\n" + str(error) + " get_iproutes: An Error has occured"
+
+
+def set_ipaddresses(auth, vlan_id, ip_address, ip_mask):
+    url_ipaddresses = 'http://{}/rest/{}/vlans/{}/ipaddresses'.format(auth.ipaddr, auth.version, vlan_id)
+    payload_ipaddress = {
+        'ip_address_mode': 'IAAM_STATIC',
+        'vlan_id': int(vlan_id),
+        'ip_address': {
+            'version': 'IAV_IP_V4',
+            'octets': ip_address
+        },
+        'ip_mask': {
+            'version': 'IAV_IP_V4',
+            'octets': ip_mask
+        }
+    }
+    payload_ipaddress = json.dumps(payload_ipaddress)
+    try:
+        r = requests.post(url_ipaddresses, headers=auth.cookie, data=payload_ipaddress)
+        return r.status_code
+    except requests.exceptions.RequestException as error:
+        return 'Error:\n {} set_ipaddresses: An Error has occured'.format(error)
+
+
+def del_ipaddresses(auth, vlan_id):
+    url_ipaddresses = 'http://{}/rest/{}/vlans/{}/ipaddresses'.format(auth.ipaddr, auth.version, vlan_id)
+    try:
+        r = requests.delete(url_ipaddresses, headers=auth.cookie)
+        return r.status_code
+    except requests.exceptions.RequestException as error:
+        return 'Error:\n {} del_ipaddresses: An Error has occured'.format(error)
